7a1b68820203d72e3a4742575cc7408052d6d6e6
Lajos Meszaros <m_lajos@hotmail.com>
2019-09-18 13:24:49 +0200

    refactor(helpers): replaced internal helpers and binders with the use of getValueAtTime()

--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
 /* global AudioParam */
 
-import { isNil } from 'ramda'
-import {
-  gotChangesScheduled,
-  getValueAtTime,
-  bindContextToParams,
-  bindSchedulerToParamMethod,
-  hijackParamValueSetter
-} from './helpers'
+import 'audioparam-getvalueattime'
+import { isNil, isEmpty, complement } from 'ramda'
+
+const isNotEmpty = complement(isEmpty)
 
 if (!isNil(window.AudioParam) && isNil(AudioParam.prototype.cancelAndHoldAtTime)) {
   if (isNil(AudioParam.prototype.cancelValuesAndHoldAtTime)) {
-    // bind all the create* functions, which create objects with at least 1 AudioParam among their properties:
-    bindContextToParams('createBiquadFilter', ['frequency', 'detune', 'Q', 'gain'])
-    bindContextToParams('createBufferSource', ['detune', 'playbackRate'])
-    bindContextToParams('createConstantSource', ['offset'])
-    bindContextToParams('createDelay', ['delayTime'])
-    bindContextToParams('createDynamicsCompressor', ['threshold', 'knee', 'ratio', 'attack', 'release'])
-    bindContextToParams('createGain', ['gain'])
-    bindContextToParams('createOscillator', ['frequency', 'detune'])
-    bindContextToParams('createPanner', ['orientationX', 'orientationY', 'orientationZ', 'positionX', 'positionY', 'positionZ'])
-    bindContextToParams('createStereoPanner', ['pan'])
-
-    // hijack param methods and mark which argument has the time
-    bindSchedulerToParamMethod('cancelScheduledValues', [0])
-    bindSchedulerToParamMethod('setValueAtTime', [1])
-    bindSchedulerToParamMethod('linearRampToValueAtTime', [1])
-    bindSchedulerToParamMethod('exponentialRampToValueAtTime', [1])
-    bindSchedulerToParamMethod('setTargetAtTime', [])
-    bindSchedulerToParamMethod('setValueCurveAtTime', [1, 2])
-
-    hijackParamValueSetter()
-
     AudioParam.prototype.cancelAndHoldAtTime = function (cancelTime) {
       const audioParam = this
-      if (gotChangesScheduled(audioParam)) {
-        const valueAtCancelTime = getValueAtTime(audioParam, cancelTime)
+      if (isNotEmpty(audioParam._scheduledChanges)) {
+        const valueAtCancelTime = audioParam.getValueAtTime(cancelTime)
 
         audioParam.cancelScheduledValues(cancelTime)
         audioParam.setValueAtTime(valueAtCancelTime, cancelTime)
